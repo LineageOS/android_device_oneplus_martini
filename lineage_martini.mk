eb8a3c7f4b7afce69ca38bc1deae65ce6f2c2181
bheatleyyy <sugakesshaa@pixelos.net>
2022-10-07 13:41:31 +0800

    martini: Rebrand to OnePlus 9RT
    Change-Id: Ic15b83bd959ed8cf3ad08dbea65c23706b2c881c

--- lineage_martini.mk
+++ lineage_martini.mk
@@ -8,26 +8,26 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from lemonade device
-$(call inherit-product, device/oneplus/lemonade/device.mk)
+# Inherit from martini device
+$(call inherit-product, device/oneplus/martini/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_lemonade
-PRODUCT_DEVICE := lemonade
+PRODUCT_NAME := lineage_martini
+PRODUCT_DEVICE := martini
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := LE2115
+PRODUCT_MODEL := MT2111
 
-PRODUCT_SYSTEM_NAME := OnePlus9
-PRODUCT_SYSTEM_DEVICE := OnePlus9
+PRODUCT_SYSTEM_NAME := MT2111_IND
+PRODUCT_SYSTEM_DEVICE := OP5155L1
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="OnePlus9-user 13 TP1A.220905.001 R.ec5be2-5994_493 release-keys" \
+    PRIVATE_BUILD_DESC="MT2111_IND-user 13 TP1A.220905.001 R.f1ca94-6ed6-6ed5 release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := OnePlus/OnePlus9/OnePlus9:13/TP1A.220905.001/R.ec5be2-5994_493:user/release-keys
+BUILD_FINGERPRINT := OnePlus/MT2111_IND/OP5155L1:13/TP1A.220905.001/R.f1ca94-6ed6-6ed5:user/release-keys
