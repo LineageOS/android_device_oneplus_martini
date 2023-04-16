bheatleyyy/device_oplus_martini

martini: Rebrand to OnePlus 9RT
Change-Id: Ic15b83bd959ed8cf3ad08dbea65c23706b2c881c

BoardConfig.mk
@@ -5,9 +5,9 @@
 #
 
 # Include the common OEM chipset BoardConfig.
-include device/oneplus/sm8350-common/BoardConfigCommon.mk
+include device/oplus/sm8350-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/oneplus/lemonade
+DEVICE_PATH := device/oplus/martini
 
 # Display
 TARGET_SCREEN_DENSITY := 450
@@ -19,4 +19,4 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
 
 # Include the proprietary files BoardConfig.
-include vendor/oneplus/lemonade/BoardConfigVendor.mk
+include vendor/oplus/martini/BoardConfigVendor.mk

device.mk
@@ -37,7 +37,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
 # Inherit from the common OEM chipset makefile.
-$(call inherit-product, device/oneplus/sm8350-common/common.mk)
+$(call inherit-product, device/oplus/sm8350-common/common.mk)
 
 # Inherit from the proprietary files makefile.
-$(call inherit-product, vendor/oneplus/lemonade/lemonade-vendor.mk)
+$(call inherit-product, vendor/oplus/martini/martini-vendor.mk)

lineage_martini.mk
@@ -9,25 +9,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from lemonade device
-$(call inherit-product, device/oneplus/lemonade/device.mk)
+$(call inherit-product, device/oplus/martini/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_lemonade
-PRODUCT_DEVICE := lemonade
+PRODUCT_NAME := lineage_martini
+PRODUCT_DEVICE := martini
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := LE2115
+PRODUCT_MODEL := MT2111
 
-PRODUCT_SYSTEM_NAME := OnePlus9
-PRODUCT_SYSTEM_DEVICE := OnePlus9
+PRODUCT_SYSTEM_NAME := OnePlus9RT
+PRODUCT_SYSTEM_DEVICE := OP5155L1
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="OnePlus9-user 12 RKQ1.211119.001 R.202210251512 release-keys" \
+    PRIVATE_BUILD_DESC="OnePlus9RT-user 12 RKQ1.211119.001 R.202209141602 release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := OnePlus/OnePlus9/OnePlus9:12/RKQ1.211119.001/R.202210251512:user/release-keys
+BUILD_FINGERPRINT := OnePlus/OnePlus9RT/OP5155L1:12/RKQ1.211119.001/R.202209141602:user/release-keys

LineageOS/android_device_oneplus_martini

BoardConfig.mk
@@ -7,7 +7,7 @@
 # Include the common OEM chipset BoardConfig.
 include device/oneplus/sm8350-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/oneplus/lemonade
+DEVICE_PATH := device/oneplus/martini
 
 # Display
 TARGET_SCREEN_DENSITY := 450
@@ -22,4 +22,4 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
 
 # Include the proprietary files BoardConfig.
-include vendor/oneplus/lemonade/BoardConfigVendor.mk
+include vendor/oneplus/martini/BoardConfigVendor.mk

device.mk
@@ -66,4 +66,4 @@ PRODUCT_SOONG_NAMESPACES += \
 $(call inherit-product, device/oneplus/sm8350-common/common.mk)
 
 # Inherit from the proprietary files makefile.
-$(call inherit-product, vendor/oneplus/lemonade/lemonade-vendor.mk)
+$(call inherit-product, vendor/oneplus/martini/martini-vendor.mk)

lineage_martini.mk
@@ -8,26 +8,26 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from lemonade device
-$(call inherit-product, device/oneplus/lemonade/device.mk)
+# Inherit from martini device
+$(call inherit-product, device/oneplus/martini/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_lemonade
-PRODUCT_DEVICE := lemonade
+PRODUCT_NAME := lineage_martini
+PRODUCT_DEVICE := martini
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := LE2115
+PRODUCT_MODEL := MT2111
 
-PRODUCT_SYSTEM_NAME := OnePlus9
-PRODUCT_SYSTEM_DEVICE := OnePlus9
+PRODUCT_SYSTEM_NAME := MT2111_IND
+PRODUCT_SYSTEM_DEVICE := OP5155L1
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="OnePlus9-user 13 TP1A.220905.001 R.ec5be2-5994_493 release-keys" \
+    PRIVATE_BUILD_DESC="MT2111_IND-user 13 TP1A.220905.001 R.f1ca94-6ed6-6ed5 release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := OnePlus/OnePlus9/OnePlus9:13/TP1A.220905.001/R.ec5be2-5994_493:user/release-keys
+BUILD_FINGERPRINT := OnePlus/MT2111_IND/OP5155L1:13/TP1A.220905.001/R.f1ca94-6ed6-6ed5:user/release-keys